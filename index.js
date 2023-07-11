0ca72d0993847cb2244107db34bce04b5710e386
Miguel Nieva <m@mikenieva.com>
2023-07-10 19:04:24 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+
+// VARIABLES
+
+const firstName = "mike"
+
+
+// SELECCIÓN DEL HTML PARA MANEJO LÓGICO
+// DOM
+
+// 1. SELECCIÓN
+const headerArea = document.querySelector("#main-header")
+const generateChangeButton = document.querySelector("#generate-change-button")
+
+console.log("generateChangeButton:", generateChangeButton)
+
+
+// 2. MANIPULACIÓN (OPCIÓN A)
+// headerArea.innerHTML = `
+//     <div>
+//         <h1>Mike Nieva</h1>
+//     </div>
+// ` 
+
+// 2. MANIPULACIÓN (OPCIÓN B)
+
+generateChangeButton.addEventListener("click", function (){
+     headerArea.innerHTML = `
+        <div>
+            <h1>Mike Nieva</h1>
+        </div>
+    `    
+})
